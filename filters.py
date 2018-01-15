d57d315f56ec979b0a8e318c34b367fcf8b3de62
Wil O. C. Ward <wil.oc.ward@gmail.com>
2018-01-15 11:27:35 +0000

    Kalman filter / smoother (RTS) & notebook

---
+++ filters.py
@@ -0,0 +1,116 @@
+""" Module for filter classes """
+
+import numpy as np
+from scipy import linalg
+import abc
+
+class _Filter(metaclass=abc.ABCMeta):
+    """
+    Abstract class for filters for predicting and updating
+    estimates in a state space model
+
+    dx(t)/dt = f(x(t), u(t), xi(t))
+    y(t) = h(x(t),u(t),nu(t))
+
+    x  : latent state
+    y  : observable state
+    u  : input/control
+    xi : process noise
+    nu : observation noise
+    ===
+    """
+    @abc.abstractmethod
+    def predict(self):
+        """ TODO: write docstring """
+        NotImplemented
+
+    @abc.abstractmethod
+    def update(self):
+        """ TODO: write docstring """
+        NotImplemented
+
+class KalmanFilter(_Filter):
+    """ """
+
+    def __init__(self,
+                 x0, P0,
+                 F = None, Q0 = None,
+                 H = None, R0 = None,
+                 Uf = None, Uh = None,
+                 _verbose:bool= False):
+        """ """
+        self.state   = {'expected': np.array(x0),
+                        'err_cov' : np.array(P0)}
+        if F is None:
+            F = np.eye(x0.shape[0])
+        if Q0 is None:
+            Q0 = np.zeros_like(F)
+        self.process = {'f': np.array(F),
+                        'U': np.array(Uf),
+                        'Q': np.array(Q0)}
+
+        if H is None:
+            H = np.eye(x0.shape[0])
+        if R0 is None:
+            R0 = np.zeros((H.shape[0], H.shape[0]))
+        self.observe = {'h': np.array(H),
+                        'U': np.array(Uh),
+                        'R': np.array(R0)}
+        self._history = {'predictions': [],
+                         'updates': []}
+        self._history['updates'].append(self.state)
+        self.initial = self.state.copy()
+        self._verbose = _verbose
+
+    def predict(self, Q = None, u=None):
+        """ """
+        x_ = self.state['expected']
+        P_ = self.state['err_cov']
+        if u is None:
+            x = self.process['f'] @ x_
+        else:
+            x = self.process['f'] @ x_ + self.process['U'] @ u
+        self.state['expected'] = x.copy()
+
+        if Q is None:
+            Q = self.process['Q']
+        else:
+            self.process['Q'] = Q
+
+        P = self.process['f'] @ P_ @ self.process['f'].T + Q
+        self.state['err_cov'] = P.copy()
+        if self._verbose:
+            print(self.state)
+        self._history['predictions'].append(self.state.copy())
+
+    def update(self, y, R = None, u = None):
+        """ """
+        if R is None:
+            R = self.observe['R']
+        else:
+            self.observe['R'] = R
+
+        P_xy = self.state['err_cov'] @ self.observe['h'].T
+        P_y  = self.observe['h'] @ P_xy + R
+
+        K = P_xy @ linalg.inv(P_y)
+
+        x_ = self.state['expected']
+        P_ = self.state['err_cov']
+
+        if u is None:
+            y_ = self.observe['h'] @ x_
+        else:
+            y_ = self.observe['h'] @ x_ + self.observe['U'] @ u
+
+        x = x_ + K @ (y - y_)
+        self.state['expected'] = x.copy()
+
+        P = P_ - K @ P_y @ K.T
+        self.state['err_cov'] = P.copy()
+
+        self._history['updates'].append(self.state.copy())
+
+
+class LinearDiscreteKalmanFilter(KalmanFilter):
+    " Alias class for Kalman Filter "
---
+++ smoothers.py
@@ -0,0 +1,91 @@
+""" Module for smooth classes """
+
+import numpy as np
+from scipy import linalg
+import abc
+from filters import _Filter
+
+class _Smoother(metaclass=abc.ABCMeta):
+    """
+    Abstract class for smoothers for smoothing filter
+    estimates in a state space model
+
+    dx(t)/dt = f(x(t), u(t), xi(t))
+    y(t) = h(x(t), u(t), nu(t))
+
+    x  : latent state
+    y  : observable state
+    u  : input/control
+    xi : process noise
+    nu : observation noise
+    ===
+    """
+    @abc.abstractmethod
+    def smooth(self):
+        """ TODO: write docstring """
+        NotImplemented
+
+class RauchTungStriebelSmoother(_Smoother):
+    """ """
+
+    def __init__(self, states,
+                 F = None, Q = None, Uf = None,
+                 _verbose:bool = False):
+        """ """
+        self.filtered = states
+        self.smoothed = [{}] * len(self.filtered)
+        self.process = {'f': np.array(F),
+                        'U': np.array(Uf),
+                        'Q': np.array(Q)}
+        self._verbose = _verbose
+        self._tx = len(self.filtered) - 1
+        self.smoothed[self._tx] = self.filtered[self._tx]
+        self._tx -= 1
+
+    @classmethod
+    def from_filter(cls, filter:_Filter):
+        " Initialise RTS smoother from filter history "
+        states = filter._history['updates']
+        F = filter.process['f']
+        Q = filter.process['Q']
+        return cls(states, F, Q)
+
+    def smooth(self):
+        """ """
+        while self._tx > -1:
+            self.smooth_incremental()
+
+        return self.smoothed
+
+
+    def smooth_incremental(self, Q = None, u = None):
+        """ """
+        if Q is None:
+            Q = self.process['Q']
+        else:
+            self.process['Q'] = Q
+
+        x_  = self.filtered[self._tx]['expected']
+
+        if u is None:
+            x_p = self.process['f'] @ x_
+        else:
+            x_p = self.process['f'] @ x_ + self.process['U'] @ u
+
+        P_  = self.filtered[self._tx]['err_cov']
+        P_p = self.process['f'] @ P_ @ self.process['f'].T + Q
+
+        S = P_ @ self.process['f'] @ linalg.inv(P_p)
+
+        x_t = self.smoothed[self._tx+1]['expected']
+        P_t = self.smoothed[self._tx+1]['err_cov']
+
+        x = x_ + S @ (x_t - x_p)
+        P = P_ + S @ (P_t - P_p) @ S.T
+
+        self.smoothed[self._tx] = {'expected': x, 'err_cov': P}
+        self._tx -= 1
+
+
+class KalmanSmoother(RauchTungStriebelSmoother):
+    """ Alias class for Rauch-Tung-Striebel Smoother """
